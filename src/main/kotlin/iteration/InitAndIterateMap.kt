0c4707110725fb2bb8556564b99961017a532ab8
mong <plto001@gmail.com>
2025-03-08 16:28:37 +0900

    add iterate test

---
+++ src/main/kotlin/iteration/InitAndIterateMap.kt
@@ -0,0 +1,13 @@
+package com.mong.iteration
+
+fun main() {
+    val binaryReps = mutableMapOf<Char, String>()
+    for (char in 'A' .. 'F') {
+        val binary = char.code.toString(radix = 2)
+        binaryReps[char] = binary
+    }
+
+    for ((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
+}
--- src/main/kotlin/iteration/IterationTest.kt
+++ src/main/kotlin/iteration/IterationTest.kt
@@ -1,5 +1,19 @@
 package com.mong.iteration
 
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun forInTest() {
+    val collection = listOf("red", "green", "blue")
+    for (color in collection) {
+        print("$color ")
+    }
+}
+
 fun main() {
     val oneToTen = 1..10
     println(oneToTen::class)
@@ -7,5 +21,17 @@ fun main() {
     oneToTen.toList().forEach(::println)
 
     // using explicit lambda
-    oneToTen.toList().forEach{i -> println(i)}
+    oneToTen.toList().forEach { i -> println(i) }
+
+    for (i in 1..100) {
+        print(fizzBuzz(i))
+    }
+    println("--------------")
+    for (i in 100 downTo 1 step 2) {
+        print(fizzBuzz(i))
+    }
+    // Buzz 98 Fizz 94 92 FizzBuzz 88 ...
+    println("--------------")
+
+    forInTest()
 }
