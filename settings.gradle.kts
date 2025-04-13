41d224be7cfffc049d9ce04f344af003e496cd5b
mong <plto001@gmail.com>
2025-04-13 19:08:41 +0900

    add comment for root project definition

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
+// rootProject.name	root project 의 이름 정의
+// include(...)	하위 project (module) 포함 설정 (multi module project 일 때)
+// pluginManagement {}	plugin repository/버전 관리
+// dependencyResolutionManagement {}	하위 project  공통 repository 설정 (Gradle 7+)
+// enableFeaturePreview(...)	실험적 Gradle 기능 활성화
 rootProject.name = "kotlin-in-action-study"
